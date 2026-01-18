0f87058733e3ba5e1adde6f2a2f29b239e22b6f5
oullavee03-ai <oullavee03@gmail.com>
2026-01-18 21:26:21 +0400

    Remove Firebase SDK import and config setup

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyDDSDsEFkaq5HnW5Be-h13fUxGkU5RciKs",
     authDomain: "our-love-app-20c4f.firebaseapp.com",
